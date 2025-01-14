9334e632f5900fbe4a5988cb6ea14b117174408a
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-14 17:15:59 +0530

    Create bubble.cpp
    bubble sort implementation

---
+++ bubble.cpp
@@ -0,0 +1,27 @@
+// naive
+void bubbleSort(arr, n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+// optimized
+
+void bubbleSort(arr, n){
+    for(int i=0; i<n-1; i++){
+        swapped = false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(swapped == false){
+            break;
+        }
+    }
+}
