c406d87caf1be1ddb181205fb5e5bf1965ec2034
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-15 21:51:23 +0530

    Update union.cpp

--- union.cpp
+++ union.cpp
@@ -1,5 +1,4 @@
 // Naive
-
 void printUnion(int a[], int b[], int m, int n){
     int c[m+n];
     for(int i=0; i<m; i++){
@@ -13,6 +12,46 @@ void printUnion(int a[], int b[], int m, int n){
     for(int i=0; i<m+n; i++){
         if(i==0 || c[i] != c[i-1]){
             cout<<c[i]<<" ";
+        }
+    }
+}
+
+// optimized
+
+void printUnion(int a[], int b[], int m, int n){
+    
+    int i=0, j=0;
+    while(i<m && j<n){
+        if(i>0 && a[i] == a[i-1]){
+            i++;
+            continue;
+        }
+        if(j>0 && b[j] == b[j-1]){
+            j++;
+            continue;
+        }
+    }
+    if(a[i] < b[j]){
+        cout<<a[i]<<" ";
+        i++;
+    }else if(a[i] > b[j]){
+        cout<<b[j]<<" ";
+        j++;
+    } else{
+        cout<<a[i]<<" ";
+        i++;
+        j++;
+    }
+    while(i<m){
+        if(i>0 && a[i] != a[i-1]){
+            cout<<a[i]<<" ";
+            i++;
+        }
+    }
+    while(j<n){
+        if(j>0 && b[j] != b[j-1]){
+            cout<<b[j]<<" ";
+            j++;
         }
     }
 }
